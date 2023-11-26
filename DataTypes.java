9ece595e1bcd33b0b7eb5b7e77f00052b7bf2402
Isuri <tmsisuri.rathnayake@gmail.com>
2023-11-26 16:36:12 +0530

    Data types

---
+++ DataTypes.java
@@ -0,0 +1,33 @@
+class DataTypes{
+    public static void main(String arg[]){
+        //primitive data types
+        // integers 
+        // byte = 1byte (8bit)
+        byte b1 = 8;
+        System.out.println(b1);
+        byte b2 = -124;
+        System.out.println(b2);
+        // short = 2byte (16bit)
+        short s = 287;
+        System.out.println(s);
+        //int = 4byte (32bit)
+        int x = 526;
+        System.out.println(x);
+        //long = 8byte (64bit)
+        long l = 253748;
+        System.out.println(l);
+        //floating points
+        // float = 4byte (32bit)
+        float f = 4.65f;
+        System.out.println(f);
+        // double = 8byte (64bit) (default)
+        double d = 8.432;
+        System.out.println(d);
+        // character = 2byte (16bit)
+        char c = 'd';
+        System.out.println(c);
+        // boolean = (1bit)
+        boolean b = true;
+        System.out.println(b);
+    }
+}
