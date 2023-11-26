71bce9c2282b2718a0a8c04830428ebf82aeb779
Isuri <tmsisuri.rathnayake@gmail.com>
2023-11-26 16:33:57 +0530

    Variables in JAVA

---
+++ Variables.java
@@ -0,0 +1,10 @@
+class Variables{
+    public static void main(String arg[]){
+        int num1 = 23;
+        int num2 = 12;
+        int result1 = num1 + num2;
+        System.out.println(result1);
+        int result2 = num1 - num2;
+        System.out.println(result2);
+    }
+}
