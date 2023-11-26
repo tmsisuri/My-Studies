b5499f77cc2176bd24e28daebd419617d9126bf7
Isuri <tmsisuri.rathnayake@gmail.com>
2023-11-26 10:19:52 +0530

    add ; mark

--- FirstCode.java
+++ FirstCode.java
@@ -1,6 +1,6 @@
 class FirstCode{
     public static void main (String arg[]){
-        System.out.println("My First JAVA Project")
+        System.out.println("My First JAVA Project");
         System.out.println("Hello World");
     }
 }
