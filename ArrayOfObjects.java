72c9dc564036aae34e3006ee15904b9daeb57347
Isuri <tmsisuri.rathnayake@gmail.com>
2023-12-08 20:29:36 +0530

    array of objects

---
+++ ArrayOfObjects.java
@@ -0,0 +1,32 @@
+class Student{
+    int rollno;
+    String name;
+    int marks;
+}
+class ArrayOfObjects {
+    public static void main(String arg[]){
+        Student s1 = new Student();
+        s1.rollno = 1;
+        s1.name = "Sara";
+        s1.marks = 89;
+
+        Student s2 = new Student();
+        s2.rollno = 1;
+        s2.name = "Jeni";
+        s2.marks = 74;
+        
+        Student s3 = new Student();
+        s3.rollno = 1;
+        s3.name = "Anne";
+        s3.marks = 63;
+        System.out.println(s3);
+        Student students[] = new Student[3];
+        students[0] = s1;
+        students[1] = s2;
+        students[2] = s3;
+        for(int i=0; i<students.length; i++){
+            System.out.println(students[i].name + " : " + students[i].marks);
+        }
+    }
+    
+}
