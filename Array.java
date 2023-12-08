725448c1c536f621b2ed0d62b55283f1ac9f0876
Isuri <tmsisuri.rathnayake@gmail.com>
2023-12-08 11:33:33 +0530

    jagged array in JAVA

--- Array.java
+++ Array.java
@@ -31,6 +31,18 @@ class Array {
             }
             System.out.println();
         }
+        //jagged array
+        int b[][] = new int[3][];
+        b[0] = new int[3];
+        b[1] = new int[2];
+        b[2] = new int[4];
+        for(int i=0; i<b.length; i++){
+            for(int j=0; j<b[i].length; j++){
+                b[i][j] = (int)(Math.random()*10);
+                System.out.print(b[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
     
 }
