4843c07e7f10c832004514b361e277cebadcc1f0
Isuri <tmsisuri.rathnayake@gmail.com>
2023-12-19 21:30:04 +0530

    add comments

--- JavaString.java
+++ JavaString.java
@@ -1,5 +1,8 @@
 class JavaString {
     public static void main(String arg[]){
+         //immutable String = data that can't chang
+        //mutable String = data that can chang
+        //String is immutable 
         String newname = new String("Hello");
         System.out.println(newname.charAt(4));
         System.out.println(newname.concat(" Sara Have A Good Day"));
@@ -9,8 +12,5 @@ class JavaString {
         String n1 = "isuri";
         String n2 = "isuri";
         System.out.println(n1 == n2);
-        //immutable String = data that can't chang
-        //mutable String = data that can chang
-        //String is immutable 
     }
 }
