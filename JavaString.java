dbca4088410e6cb387577b9fe50e2605c63716d7
Isuri <tmsisuri.rathnayake@gmail.com>
2023-12-20 21:23:26 +0530

    StringBuffer and StringBuilder in JAVA

--- JavaString.java
+++ JavaString.java
@@ -1,10 +1,10 @@
 class JavaString {
     public static void main(String arg[]){
-         //immutable String = data that can't chang
+        //immutable String = data that can't chang
         //mutable String = data that can chang
         //String is immutable 
         String newname = new String("Hello");
-        System.out.println(newname.charAt(4));
+        System.out.println(newname.charAt(3));
         System.out.println(newname.concat(" Sara Have A Good Day"));
 
         String name = " Sara ";
@@ -12,5 +12,13 @@ class JavaString {
         String n1 = "isuri";
         String n2 = "isuri";
         System.out.println(n1 == n2);
+
+        //StringBuffer and StringBuilder
+        //difference between StringBuffer and StringBuilder
+        //StringBuffer is thread safe and StringBuilder is not
+        StringBuffer sb = new StringBuffer("JAVA");
+        System.out.println(sb);
+        System.out.println(sb.capacity());
+        System.out.println(sb.length());
     }
 }
