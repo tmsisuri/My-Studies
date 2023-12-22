4d8b358c7429b8d0311c28913630620868f5168f
Isuri <tmsisuri.rathnayake@gmail.com>
2023-12-22 19:42:30 +0530

    simple calculator two numbers

---
+++ Calculator.java
@@ -0,0 +1,35 @@
+ import java.util.Scanner;
+class Calculator {
+    public static void main(String arg[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("1-Add");
+        System.out.println("2-Subtract");
+        System.out.println("3-Multiply");
+        System.out.println("4-Divide");
+        System.out.println("Choose operator : ");
+        int operator = sc.nextInt();
+        System.out.println("Enter first number : ");
+        int n1 = sc.nextInt();
+        System.out.println("Enter second number : ");
+        int n2 = sc.nextInt();
+        int result = 0;
+        switch(operator){
+            case 1 :
+            result = n1 + n2;
+            break;
+            case 2 :
+            result = n1 - n2;
+            break;
+            case 3 :
+            result = n1 * n2;
+            break;
+            case 4 :
+            result = n1 / n2;
+            break;
+            default :
+            System.out.println("invalid operator");
+        }
+        System.out.println(result);
+        sc.close();
+    }
+}
