a2e509bc069882bb597cfa781cfa5bec1adf8807
Isuri <tmsisuri.rathnayake@gmail.com>
2023-12-29 13:23:45 +0530

    add this keyword

--- Encapsulation.java
+++ Encapsulation.java
@@ -4,14 +4,14 @@ class Students{
     public String getName(){
         return name;
     }
-    public void setName(String n){
-        name = n;
+    public void setName(String name){
+        this.name = name;
     }
     public int getAge(){
         return age;
     }
-    public void setAge(int a){
-        age = a;
+    public void setAge(int age){
+        this.age = age;
     }
 }
 class Encapsulation {
