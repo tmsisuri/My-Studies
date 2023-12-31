1001036c7ba28eb378920e49f618a48b8428e4cc
Isuri <tmsisuri.rathnayake@gmail.com>
2023-12-31 22:07:27 +0530

    parameterized constructors

--- Constructor.java
+++ Constructor.java
@@ -1,8 +1,18 @@
 class Drinks{
     private String name;
     private int price;
+    //default constructor
     public Drinks(){
         name = "drinks";
+        price = 100;
+    }
+    //parameterized constructor
+    public Drinks(String name , int price){
+        this.name = name;
+        this.price = price;
+    }
+    public Drinks(String name) {
+        this.name = name;
         price = 100;
     }
     public String getName(){
@@ -26,5 +36,9 @@ class Constructor {
         obj2.setName("Coffee");
         obj2.setPrice(150);
         System.out.println(obj2.getName() + " : " + obj2.getPrice());
+        Drinks obj3 = new Drinks("Hot chocolate" , 200 );
+        System.out.println(obj3.getName() + " : " + obj3.getPrice());
+        Drinks obj4 = new Drinks("Ice Coffee");
+        System.out.println(obj4.getName() + " : " + obj4.getPrice());
     }
 }
