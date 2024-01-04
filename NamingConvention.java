7d08543b7e0e22f20fbe004ff1bb8787ddc2b5e3
Isuri <tmsisuri.rathnayake@gmail.com>
2024-01-04 21:47:22 +0530

    naming convention in JAVA

---
+++ NamingConvention.java
@@ -0,0 +1,18 @@
+class NamingConvention {
+    public static void main(String arg[]){
+        //camel casing
+
+        //class and interface - first letter capital ( Calculator , Runable)
+        //variable - first letter simple ( name )
+        //method - first letter simple and round bracket in the end ( students() )
+        //constructor - first letter capital and round bracket in the end ( Drinks() )
+        //constants - all letters capital ( BRAND )
+        
+        //more than one word - the first letter of the second or subsequent words are capital
+        //MyCalculator
+        //studentName
+        //myCar()
+        //FoodAndDrinks()
+        //NEW_BRAND
+    }    
+}
