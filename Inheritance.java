5e1cabb894787c7f8435a3831ec6a65e169f56df
Isuri <tmsisuri.rathnayake@gmail.com>
2024-01-08 21:58:59 +0530

    inheritance in JAVA

---
+++ Inheritance.java
@@ -0,0 +1,26 @@
+class Calc{
+    public int add(int n1 , int n2){
+        return n1 + n2;
+    }
+    public int sub(int n1 , int n2){
+        return n1 - n2;
+    }
+}
+class AdvCalc extends Calc{
+    public int multi(int n1 , int n2){
+        return n1 * n2;
+    }
+    public int div(int n1 , int n2){
+        return n1 / n2;
+    }
+}
+class Inheritance {
+    public static void main(String arg[]){
+        AdvCalc obj = new AdvCalc();
+        int r1 = obj.add(4, 8);
+        int r2 = obj.sub(7, 3);
+        int r3 = obj.multi(5,8);
+        int r4 = obj.div(15,3);
+        System.out.println(r1 + " " + r2 + " " + r3 + " " + r4);
+    }
+}
