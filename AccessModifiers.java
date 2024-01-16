32b27f3f4fb6e338cbdd01bb002c00863b1822f8
Isuri <tmsisuri.rathnayake@gmail.com>
2024-01-16 22:15:35 +0530

    access modifiers in JAVA

---
+++ AccessModifiers.java
@@ -0,0 +1,18 @@
+public class AccessModifiers {
+    public static void main(String arg[]){
+
+        //private   - declarations are visible within the class only
+        //            (only in same class)
+        //default   - declarations are visible only within the package (package private)
+        //            (same class , package)
+        //protected - declarations are visible within the package or all subclasses
+        //            (same class , subclass , package)
+        //public    - declarations are visible everywhere
+        //            (everyone)
+
+        //class     - public
+        //method    - most of the time public
+        //variables - private 
+        
+    }
+}
