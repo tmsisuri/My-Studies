4a15c1df3f531550f7a7ef699d039600f56e4eec
Isuri <tmsisuri.rathnayake@gmail.com>
2024-02-06 12:33:49 +0530

    int to long data type change

--- FactorialNumbers.java
+++ FactorialNumbers.java
@@ -3,9 +3,9 @@ import java.util.Scanner;
 public class FactorialNumbers {
     public static void main(String arg[]){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter number : ");
+        System.out.print("Enter number : ");
         int n = sc.nextInt();
-        int result = 1;
+        long result = 1;
         for(int count=1 ; count<=n ; count++){
             result = result * count;   
         }
