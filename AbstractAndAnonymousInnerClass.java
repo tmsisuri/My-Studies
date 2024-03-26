23d0204254d428362e37f0f4e8bf4c2e42320555
Isuri <tmsisuri.rathnayake@gmail.com>
2024-03-26 20:55:51 +0530

    fix lines

--- AbstractAndAnonymousInnerClass.java
+++ AbstractAndAnonymousInnerClass.java
@@ -7,15 +7,15 @@ abstract class A{
 public class AbstractAndAnonymousInnerClass {
     public static void main(String[] args){
         //this will create a object of anonymous inner class(not create a object of A class(becouse A is a abstract class))
-       A obj = new A(){
-        public void show(){
-            System.out.println("in new show");
-        }
-        public void config(){
-            System.out.println("in config");
-        }
-       };
-       obj.show();
-       obj.config();
+        A obj = new A(){
+            public void show(){
+                System.out.println("in new show");
+            }
+            public void config(){
+                System.out.println("in config");
+            }
+        };
+        obj.show();
+        obj.config();
     }
 }
