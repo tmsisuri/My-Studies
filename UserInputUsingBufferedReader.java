5c2c84c3e31eabb6237f680a3a05307d678737b5
isuri rathnayake <tmsisuri.rathnayake@gmail.com>
2024-05-30 12:44:07 +0530

    user input using BufferedReader in java

---
+++ UserInputUsingBufferedReader.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class UserInputUsingBufferedReader {
+    public static void main(String[] args) {
+        System.out.print("Enter a number : ");
+
+        InputStreamReader in = new InputStreamReader(System.in);
+        BufferedReader bf = new BufferedReader(in);
+
+        int num = 0;
+        
+        try {
+            num = Integer.parseInt(bf.readLine());
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(num);
+    }
+}
