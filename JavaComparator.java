5ecb13c13f65f1050d85d96dfad57568cf0a2e74
isuri rathnayake <tmsisuri.rathnayake@gmail.com>
2024-06-22 20:02:28 +0530

    comparator in java

--- JavaComparator.java
+++ JavaComparator.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class JavaComparator {
@@ -15,5 +16,25 @@ public class JavaComparator {
     Collections.sort(nums);
     System.out.println(nums);
  
+    //sort values based on your own logic
+    Comparator<Integer> com = new Comparator<Integer>() {
+      public int compare(Integer i , Integer j) {
+        if (i%10 > j%10)
+            return 1;
+        else if (i%10 < j%10)
+            return -1;
+        else
+            return 0;
+      }
+    };
+    List<Integer> nums2 = new ArrayList<>();
+    nums2.add(43);  
+    nums2.add(31);   
+    nums2.add(72);   
+    nums2.add(29);  
+
+    Collections.sort(nums2, com);
+    System.out.println(nums2);
+
   }
 }
