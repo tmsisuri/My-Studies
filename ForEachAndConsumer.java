e08d8434d9384605379d832db7cccecd958d9f5f
isuri rathnayake <tmsisuri.rathnayake@gmail.com>
2024-06-23 20:51:30 +0530

    forEach method and Consumer in java

---
+++ ForEachAndConsumer.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class ForEachAndConsumer {
+    public static void main(String[] args) {
+        List<Integer> nums = Arrays.asList(3,6,5,2,8,7,4);
+
+        Consumer<Integer> con = new Consumer<Integer>() {
+            public void accept(Integer n) {
+                System.out.println(n);
+            }
+        };
+        nums.forEach(con);
+
+        //woth lambda Expression
+        Consumer<Integer> consumer = n -> System.out.println(n);
+        nums.forEach(consumer);
+
+        //without creating consumer object
+        nums.forEach(x -> System.out.println(x));
+    }
+}
